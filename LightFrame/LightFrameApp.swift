9f05555c8287773a819206d033d6d4de0ed75dcf
Dan Radigan <dan@danradigan.com>
2026-03-14 09:22:55 -0700

    Initial Commit

---
+++ LightFrame/LightFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightFrameApp.swift
+//  LightFrame
+//
+//  Created by Dan Radigan on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct LightFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
